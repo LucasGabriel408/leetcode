1d420788d65ff73a6e38d57a9159488bc4593758
Lucas Gabriel <142439949+LucasGabriel408@users.noreply.github.com>
2026-04-24 19:24:05 -0300

    Optimize twoSum function using hash map
    Refactored the twoSum function to use a hash map for improved efficiency.

--- python/easy/two_sum.py
+++ python/easy/two_sum.py
@@ -1,17 +1,23 @@
-# Two Sum - Easy
+# Problem: Two Sum
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/two-sum/
+#
+# Description:
+# Given an array of integers nums and an integer target,
+# return indices of the two numbers that add up to target.
+# You may assume that each input would have exactly one solution,
+# and you may not use the same element twice.
+#
+# Approach:
+# Hash map to store previously seen numbers and their indices.
+# For each number, check if its complement (target - num) already exists.
+# Time: O(n) | Space: O(n)
 
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-solution = Solution()
-result = solution.twoSum([2,7,11,15], 9)
-print(result)
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
