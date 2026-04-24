010bd04b39964ed267c91fcee03c75765c50166e
Lucas Gabriel <142439949+LucasGabriel408@users.noreply.github.com>
2026-04-24 19:30:13 -0300

    Refactor median calculation and add problem details
    Refactor median calculation to use list concatenation instead of numpy. Add problem description and TODO for binary search implementation.

--- python/hard/median_of_two_sorted_arrays.py
+++ python/hard/median_of_two_sorted_arrays.py
@@ -1,19 +1,25 @@
-# Median of Two Sorted Arrays - Hard
+# Problem: Median of Two Sorted Arrays
+# Difficulty: Hard
+# Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+#
+# Description:
+# Given two sorted arrays nums1 and nums2 of size m and n respectively,
+# return the median of the two sorted arrays.
+# The overall run time complexity should be O(log(m+n)).
+#
+# Approach (current - to be refactored):
+# Concatenate both arrays using numpy, sort and calculate the median.
+# This works but uses an external library and does not meet the expected
+# O(log(m+n)) complexity. Needs to be refactored using binary search.
+# Time: O((m+n) log(m+n)) | Space: O(m+n)
+#
+# TODO: Refactor using binary search to achieve O(log(m+n))
 
-
-import numpy as np
-
-class Solution(object):
+class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        nums = np.concatenate((nums1, nums2))
+        nums = list(nums1) + list(nums2)
         nums.sort()
-        return np.median(nums)
-
-solution = Solution()
-result = solution.findMedianSortedArrays([1,3], [2,5])
-print(result)
+        n = len(nums)
+        if n % 2 == 1:
+            return float(nums[n // 2])
+        return (nums[n // 2 - 1] + nums[n // 2]) / 2.0
